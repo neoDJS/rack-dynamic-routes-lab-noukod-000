982adb9ec3de22e15a0eb5e9f6e66e4d20b87cc4
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-04 18:28:25 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,62 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/) 
+      item_name = req.path.split("/items/").last #turn /items/Sorry into Sorry
+      item = @@items.find{|i| i.name == item_name}
+ 
+      resp.write item.price
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+<<<<<<< HEAD
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+=======
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+>>>>>>> 2770e4a091fe8ae67ae79201c58f61595bdac3e4
+      end
+    elsif req.path.match(/add/)
+      cart_item = req.params["item"]
+      resp.write handle_add(cart_item)
+    else
+      resp.write "Path Not Found"
+    end
+
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+
+  def handle_add(cart_item)
+    if @@items.include?(cart_item)
+      @@cart << cart_item
+<<<<<<< HEAD
+      return "added #{cart_item}"
+    else
+      return "We don't have that item"
+=======
+      return "#{cart_item} is added to your cart"
+    else
+      return "Couldn't find #{cart_item}"
+>>>>>>> 2770e4a091fe8ae67ae79201c58f61595bdac3e4
+    end
+  end
+end
