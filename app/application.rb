0a61ad67f77e1b519bf0176d33cdc7188925ca92
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-04 18:43:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/*").last #turn /items/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
+      puts @@items
       if item.nil?
         resp.write "Item not found"
         resp.status = 400
