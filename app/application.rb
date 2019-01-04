042934044914fba229fc291dc2e03f1c4e268133
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-04 18:30:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,30 +6,11 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/) 
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last #turn /items/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
- 
+
       resp.write item.price
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-<<<<<<< HEAD
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
-=======
-      @@cart.each do |item|
-        resp.write "#{item}\n"
->>>>>>> 2770e4a091fe8ae67ae79201c58f61595bdac3e4
-      end
-    elsif req.path.match(/add/)
-      cart_item = req.params["item"]
-      resp.write handle_add(cart_item)
     else
       resp.write "Path Not Found"
     end
