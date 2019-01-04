e29fb41089a42c50dea78767ff2f5fbe0f3dd60b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-04 18:36:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,27 +22,4 @@ class Application
 
     resp.finish
   end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-
-  def handle_add(cart_item)
-    if @@items.include?(cart_item)
-      @@cart << cart_item
-<<<<<<< HEAD
-      return "added #{cart_item}"
-    else
-      return "We don't have that item"
-=======
-      return "#{cart_item} is added to your cart"
-    else
-      return "Couldn't find #{cart_item}"
->>>>>>> 2770e4a091fe8ae67ae79201c58f61595bdac3e4
-    end
-  end
 end
