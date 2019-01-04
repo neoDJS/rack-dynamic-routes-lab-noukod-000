1ef70d654a6b99b6820a4c69427368fa1e1e1be1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-04 18:32:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last #turn /items/Sorry into Sorry
+      item_name = req.path.split("/items/*").last #turn /items/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
 
       resp.write item.price
